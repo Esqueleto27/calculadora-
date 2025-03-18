039613c082ed030a3cae5646930f1ce89d7ecda3
Esqueleto27 <matheofloresloor@gmail.com>
2025-03-18 14:08:36 -0500

    Agregamos las funciones basicas

--- adicional/operaciones.py
+++ adicional/operaciones.py
@@ -1,1 +1,21 @@
 # Operaciones basicas
+
+
+# Suma
+def suma(num1, num2):
+    return num1 + num2
+
+
+# Resta
+def Resta(num1, num2):
+    return num1 - num2
+
+
+# Multiplicacion
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+
+# Division
+def division(num1, num2):
+    return num1 / num2
