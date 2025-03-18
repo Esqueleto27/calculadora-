84a813f74896af4258393aaa4f9f29941a252d55
Esqueleto27 <matheofloresloor@gmail.com>
2025-03-18 14:12:28 -0500

    Agregamos las interacciones

--- adicional/interacciones.py
+++ adicional/interacciones.py
@@ -1,1 +1,13 @@
 # Interacciones con el usuario
+
+
+def saludo():
+    print(" ")
+    print("- - - - - Bienvendios a la calculadora de Matheo - - - - - ")
+    print(" ")
+
+
+def despedida():
+    print(" ")
+    print("- - - - - Gracias por usar la calculadora de Matheo - - - - - ")
+    print(" ")
