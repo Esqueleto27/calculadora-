91735a3f863bf3f8d368edf00e38c7cc3e964fbf
Esqueleto27 <matheofloresloor@gmail.com>
2025-03-18 17:16:53 -0500

    integracion de while

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 # App de calculadora
 
 from adicional.operaciones import suma, resta, multiplicacion, division
-from adicional.interacciones import saludo, despedida
+from adicional.interacciones import saludo, despedida, menu
 
-operacionm = input("Que operacion desea hacer: ")
+saludo()
 
-num1 = int(input("Ingrese su primer numero: "))
-num2 = int(input("Ingrese su segundo numero: "))
+while True:
+    menu()
+    operaciones = input("Que operacion desea hacer: ")
+    if operaciones in ("salir", "5"):
+        break
+
+    num1 = int(input("Ingrese su primer numero: "))
+    num2 = int(input("Ingrese su segundo numero: "))
+
+    if operaciones == "suma":
+        print(suma())
+    elif operaciones == "resta":
+        print(resta())
+    elif operaciones == "multiplicacion":
+        print(multiplicacion())
+    elif operaciones == "division":
+        print(division())
