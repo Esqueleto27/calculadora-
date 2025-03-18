cd577132ecea33d2a5f00adde92e8fb567e1805b
Esqueleto27 <matheofloresloor@gmail.com>
2025-03-18 17:12:45 -0500

    Agregamos la parte de menu

--- adicional/interacciones.py
+++ adicional/interacciones.py
@@ -7,6 +7,16 @@ def saludo():
     print(" ")
 
 
+def menu():
+    print(" - - - - - - - - - - - - - - - - - - - -")
+    print("1. Suma ")
+    print("2. Resta ")
+    print("3. Multiplicacion")
+    print("4. Division")
+    print("5. Salir ")
+    print(" - - - - - - - - - - - - - - - - - - - -")
+
+
 def despedida():
     print(" ")
     print("- - - - - Gracias por usar la calculadora de Matheo - - - - - ")
